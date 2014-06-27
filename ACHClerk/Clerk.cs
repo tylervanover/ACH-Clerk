451239a5715dbd5e698ba9cf3f15fecfb63f9cc0
tylervanover <yeti.jones@gmail.com>
2014-06-27 11:42:26 -0500

    Started fleshing out the Clerk.cs.

--- ACHClerk/Clerk.cs
+++ ACHClerk/Clerk.cs
@@ -13,32 +13,49 @@ namespace ACHClerk
     {
         /// <summary>
         /// Documents to be printed as the final ACH packet. PacketEntry
-        /// is a custom class, which will be generated soon.
+        /// is a custom class, representing the PdfDocument, and various tags.
+        /// The tag implementation (such as "service: gas" or "company: WestStar") is
+        /// still under construction.
         /// </summary>
         private List<PacketEntry> _packetDocuments;
 
         /// <summary>
-        /// Using PDFSharp library to manage PDF manipulation.
+        /// A complete collection of the native change form PDFs. From this selection, the final _packetDocuments
+        /// will be compiled, and then printed.
         /// </summary>
-        private List<PdfDocument> _nativeChangeForms;
-
-        private String _loadPath;
+        private List<PacketEntry> _nativeChangeForms;
 
         /// <summary>
-        /// Default, public constructor. More to come.
+        /// The path representing the parent directory of the collection of native change forms.
+        /// The parent directory will then contain numerous subdirectories, which will be walked
+        /// recursively to load the actual PDF, and to read the various tags associated to each.
         /// </summary>
-        public Clerk(String Path)
+        private String _parentDirectory;
+
+        /// <summary>
+        /// Default, public constructor. Sets the parent directory, and allocates memory
+        /// for the class' collections. 
+        /// </summary>
+        public Clerk(String loadPath)
         {
-            LoadPath = Path;
+            ParentDirectory = loadPath;
             _packetDocuments = new List<PacketEntry>();
-            _nativeChangeForms = new List<PdfDocument>();
+            _nativeChangeForms = new List<PacketEntry>();
         }
 
+        /// <summary>
+        /// Loads the native Change forms. They are all PDF documents that contain
+        /// information on switching ACH transfers between financial institutions.
+        /// The method is going to first check the load path, which is an initial directory.
+        /// Next, it will recursively walk through each subdirectory, each one representing a separate 
+        /// company's ACH form, and then a quick text file that contains the tags for "service", "company".
+        /// </summary>
+        /// <param name="path"></param>
+        /// <returns></returns>
         private bool LoadNativeChangeForms(String path)
         {
             if (Directory.Exists(path))
             {
-                String[] files = Directory.GetFiles(path);
                 return true;
             }
             else
@@ -47,15 +64,18 @@ namespace ACHClerk
             }
         }
 
-        public String LoadPath
+        /// <summary>
+        /// Member access modifier. 
+        /// </summary>
+        public String ParentDirectory
         {
             get
             {
-                return _loadPath;
+                return _parentDirectory;
             }
             set
             {
-               _loadPath = value;
+                _parentDirectory = value;
             }
         }
     }
