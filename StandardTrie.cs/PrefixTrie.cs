097469bd6772f5682f7bbec1193c7a15153a296c
tylervanover <yeti.jones@gmail.com>
2014-07-04 17:17:26 -0500

    Fixed a file name error for PrefixTrieNode.cs. Also, started implementing the Insertion method within PrefixTrie.cs, or, rather, the structures it will use to reduce the overall complexity of the method.

--- StandardTrie.cs/PrefixTrie.cs
+++ StandardTrie.cs/PrefixTrie.cs
@@ -6,15 +6,32 @@ using System.Threading.Tasks;
 
 namespace StandardTrie.cs
 {
+    // Shorten up the typing, just a tad.
+    using PFTNode = StandardTrie.cs.PrefixTrieNode;
+
     /// <summary>
-    /// The parent class for a Prefix Trie structure. Contained within the StandardTrie namespace.
-    /// 
-    /// This variation of a Trie will allow the user to search each node using prefixes to strings.
-    /// It will help with an auto-complete function, such that only portions of a string can be searched.
+    /// The PrefixTrie structure. This class will handle the manipulation of
+    /// nodes within the trie. It has a single root node, from which the
+    /// rest of the insertion, contains, and removal methods can be initiated.
     /// </summary>
     public class PrefixTrie
     {
-        public PrefixTrieNode RootNode { get; private set; }
+        /// <summary>
+        /// The root node of the tree.
+        /// </summary>
+        public PFTNode RootNode { get; private set; }
+
+        /// <summary>
+        /// Alphabet used to index children from.
+        /// </summary>
+        private const char[] alphabet = {  'a', 'b', 'c', 'd', 'e', 
+                                        'f', 'g', 'h', 'i', 'j', 
+                                        'k', 'l', 'm', 'n', 'o',
+                                        'p', 'q', 'r', 's', 't',
+                                        'u', 'v', 'w', 'x', 'y',
+                                        'z' };
+
+        private Grid lookup;
 
         /// <summary>
         /// Public, non-default constructor.
@@ -23,7 +40,44 @@ namespace StandardTrie.cs
         {
             // Create a new root node. Use the constant within PrefixTrieNode to
             // set the root node's Letter. 
-            RootNode = new PrefixTrieNode { Letter = PrefixTrieNode.Root };
+            RootNode = new PFTNode { Letter = PFTNode.RootChar, IsEnd = false };
+            lookup = BuildLookup(alphabet);
+        }
+
+        /// <summary>
+        /// The entry point insertion method. This will then call a private, recursive function
+        /// to finish the insertion of the word. 
+        /// </summary>
+        /// <param name="word"></param>
+        /// <returns></returns>
+        public bool Insert(string word)
+        {
+            int childIndex = lookup[word[0]];
+            throw new NotImplementedException();
+        }
+
+        /// <summary>
+        /// A recursive function to insert a word into the PrefixTrie. It must be called
+        /// by the entry function, Insert. The function will continue to build the word
+        /// path along the PrefixTrie, and then will return the primary node to the entry
+        /// function. 
+        /// </summary>
+        /// <param name="root"></param>
+        /// <param name="word"></param>
+        /// <returns></returns>
+        private PFTNode rInsert(PFTNode root, string word)
+        {
+            throw new NotImplementedException();
+        }
+
+        /// <summary>
+        /// Builds a looup for the alphabet.
+        /// </summary>
+        /// <param name="values"></param>
+        /// <returns></returns>
+        private Grid BuildLookup(char[] values)
+        {
+            return new Grid(values);
         }
     }
 }
---
+++ StandardTrie.cs/PrefixTrieNode.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Collections.Specialized;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StandardTrie.cs
+{
+    /// <summary>
+    /// A node of a prefix tree. The prefix tree will allow the user
+    /// to build a structure of characters such that they can perform traversals
+    /// of complete words, and of prefixes to words.
+    /// </summary>
+    public class PrefixTrieNode
+    {
+        public const char RootChar = ' ';
+        public const char EOW = '$';
+
+        public char Letter { get; set; }
+        public bool IsEnd { get; set; }
+
+        public PrefixTrieNode[] Children;
+
+        public PrefixTrieNode()
+        {
+
+        }
+
+        public PrefixTrieNode(char c, bool end)
+        {
+            Letter = c;
+            IsEnd = end;
+            Children = new PrefixTrieNode[26];
+        }
+
+        public PrefixTrieNode this[int index]
+        {
+            get
+            {
+                return Children[index];
+            }
+        }
+    }
+}
